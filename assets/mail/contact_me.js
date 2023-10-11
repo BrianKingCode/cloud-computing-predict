bdc7fa4534d2ff150c898696b16bb26a50025021
Brian Kgatla <94054959+BrianKingCode@users.noreply.github.com>
2023-10-11 15:55:55 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: " https://4cb5sqgh1i.execute-api.eu-west-1.amazonaws.com/default/brian_kgatla-Lambda-Function",
+                url: "https://4cb5sqgh1i.execute-api.eu-west-1.amazonaws.com/default/brian_kgatla-Lambda-Function",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
